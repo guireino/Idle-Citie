615477804e7775e4cb7050c6e2db2ea6367aa273
guilherme <guiabucarma@gmail.com>
2023-06-27 18:04:21 -0400

    add os arquivos projeto 27/06/23

---
+++ Assets/Scripts/slotController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class slotController : MonoBehaviour
+{
+
+    public GameObject baseHud, hudActive, hudBuy, hudUpgrade, iconCoin;
+    public Image loadProduction;
+    public TMP_Text txtProduction;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
